037ef1f54f5910dc84319784f27e958858a676ab
ArielPerez2001 <159938572+ArielPerez2001@users.noreply.github.com>
2024-02-20 09:43:52 -0700

    Actividad 13

--- Unidad1_ProgramacionIII/src/Window/Ventana.java
+++ Unidad1_ProgramacionIII/src/Window/Ventana.java
@@ -1,10 +1,75 @@
 package Window;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 
-public class Ventana {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Main window = new Main();
+public class Ventana extends JFrame{
+	
+	public Ventana() {
+		
+	
+		this.setSize(1000,500);
+		
+		this.setVisible(true);
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.setLocation(200,100);
+				
+		this.setResizable(true);
+		
+		this.setTitle("Mi ventana");
+		
+		this.setMinimumSize(new Dimension(250,250));
+		
+		this.setMaximumSize(new Dimension(1000,750));
+		
+		this.setLocationRelativeTo(null);
+		
+		this.iniciarComponentes();
+		
+		this.setLayout(null);//Conserva los tamaños asignados
 	}
-
+	
+	//Metodo para crear el panel
+	public void iniciarComponentes() {
+		JPanel login = new JPanel();
+		login.setSize(this.getWidth()/2,this.getHeight());
+		login.setBackground(Color.BLUE);
+		login.setLayout(null);
+		this.add(login);
+		
+		//Componente para agregar string
+		JLabel login_tag = new JLabel("Acceder",0);
+		login_tag.setSize(300, 80);
+		login_tag.setFont(new Font ("Arial",Font.BOLD,24));
+		login_tag.setForeground(Color.white);
+		login_tag.setLocation(100, 20);
+		login_tag.setOpaque(true);//Fondo a la etiqueta
+		login_tag.setBackground(Color.red);
+		login.add(login_tag);
+		
+		
+		
+		//Reglas generales de cada componente
+		
+		//Size
+		//location
+		//background
+		//string constructor
+		
+		/*JPanel register = new JPanel();
+		login.setSize(this.getWidth()/2,this.getHeight());
+		register.setLocation(500, 0);
+		login.setBackground(Color.GREEN);
+		
+		this.add(register);/*/
+	}
+	
+	
+	
 }
